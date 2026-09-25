ffbca4df3ffef9073551f5c0973430e1149280b7
Aditya-Rajput-00 <adityasinghstudent2002@gmail.com>
2026-09-25 10:26:19 +0530

    chore: configure environment variables

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/campusfind");
+        await mongoose.connect(process.env.MONGO_URI);
 
         console.log("MongoDB connected successfully");
     } catch (error) {
